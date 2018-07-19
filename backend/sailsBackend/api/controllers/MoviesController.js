dc09c2f279a2289324ac3dfddcacafdcc011dcc3
taymeolu <temidayo.o@ng.lopworks.com>
2018-07-19 08:36:37 +0100

    included a route for add movies.

--- backend/sailsBackend/api/controllers/MoviesController.js
+++ backend/sailsBackend/api/controllers/MoviesController.js
@@ -6,7 +6,15 @@
  */
 
 module.exports = {
-  
+  add: function(req, res){
+      var url = '';
+      fetch(url)
+        .then((resp) => resp.json())
+        .then(function(data){
+            var results = data.results;
+            
+        })
+  }
 
 };
 
