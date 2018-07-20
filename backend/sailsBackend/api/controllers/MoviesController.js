2039dbe36d4a85db6928725b64822b6d3d7765f7
taymeolu <temidayo.o@ng.lopworks.com>
2018-07-20 09:42:39 +0100

    modified movies controller, addedd image links to payload and changed search paramter to latest movies

--- backend/sailsBackend/api/controllers/MoviesController.js
+++ backend/sailsBackend/api/controllers/MoviesController.js
@@ -8,7 +8,7 @@
 module.exports = {
     search: function (req, res) {
         var fetch = require('node-fetch');
-        var url = 'https://api.themoviedb.org/3/search/movie?api_key=f153a168a75fb9dd9e689bbb3b502d40&language=en-US&query=a';
+        var url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=f153a168a75fb9dd9e689bbb3b502d40&language=en-US';
         
         fetch(url)
         .then(function (response) {
@@ -23,12 +23,19 @@ module.exports = {
                 movie.id = element.id;
                 movie.title = element.title;
                 movie.overview = element.overview;
+                movie.image = 'https://image.tmdb.org/t/p/original' + element.poster_path;
                 movie.price = Number.parseFloat((Math.random() * 1000).toFixed(2));
 
                 results.push(movie);
             });
 
             res.send(results);
+        })
+        .catch(function (){
+            var result = {};
+            result.id = 99;
+            result.message = 'Could not fetch';
+            res.send(result);
         });
     }
 
