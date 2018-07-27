6a636dcb0517334af1f4ea02ed38715124aff920
temidayo.o@ng.lopworks.com <temidayo.o@ng.lopworks.com>
2018-07-27 16:32:15 +0100

    added jumbotron, basic webpage

--- frontend/vuefrontend/src/App.vue
+++ frontend/vuefrontend/src/App.vue
@@ -1,24 +1,27 @@
 <template>
   <div>
-    <b-navbar toggleable="md" type="dark" variant="info">
+    <b-navbar toggleable="md" type="dark" variant="dark">
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
       <b-navbar-brand href="#">MovieRentals</b-navbar-brand> 
     </b-navbar>
+    <Jumbotron></Jumbotron>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Jumbotron from './components/Jumbotron.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Jumbotron,
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+  body{
+    background: #344957;
+  }
 </style>
---
+++ frontend/vuefrontend/src/components/Jumbotron.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <b-jumbotron class ="content" header="Rent Movies Here!">
+
+    </b-jumbotron>
+    <h1>Latest Movies</h1>
+    <div>
+      <b-container fluid>
+        <b-row>
+          <b-col sm="6" md="4" v-for="movie in movies" :key="movie.id">
+            <img :src="movie.image" alt="" srcset="">
+            <b-button>Only ₦{{movie.price}}!</b-button>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
+    <!-- <ul>
+      <li v-for="movie in movies" :key="movie.id">
+        <img :src="movie.image" alt="" srcset="">
+      </li>
+    </ul> -->
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Jumbotron",
+  data() {
+    return {
+      movies: [],
+      errors: []
+    };
+  },
+
+  created() {
+    var axios = require('axios')
+    axios
+      .get(`http://localhost:1337/search`)
+      .then(response => {
+        this.movies = response.data;
+      })
+      .catch(e => {
+        this.errors.push(e);
+      });
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.content {
+  margin-top: 1%;
+  background-image: url("../assets/popcorn.jpg");
+}
+h1 {
+  text-align: center;
+  margin-bottom: 2%;
+}
+img{
+  max-height: 100%;
+  max-width: 100%;
+  margin-top: 2%;
+  margin-bottom: 2%;
+}
+button{
+  align-self: auto;
+}
+</style>
