4ef75055bb727dff0316b1996cb27afbf3165407
temidayo.o@ng.lopworks.com <temidayo.o@ng.lopworks.com>
2018-07-30 14:18:15 +0100

    Moved navbar into its own component

--- frontend/vuefrontend/src/App.vue
+++ frontend/vuefrontend/src/App.vue
@@ -1,42 +1,19 @@
 <template>
   <div id="body">
-    <b-navbar toggleable="md" type="dark" variant="dark">
-      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-
-      <b-navbar-brand href="#">MovieRentals <font-awesome-icon icon="coffee" /></b-navbar-brand> 
-        <b-collapse is-nav id="nav_collapse">
-
-          <!-- Right aligned nav items -->
-          <b-navbar-nav class="ml-auto">
-
-            <b-nav-form>
-              <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
-              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-            </b-nav-form>
-
-            <b-nav-item-dropdown right>
-              <!-- Using button-content slot -->
-              <template slot="button-content">
-                <em>User</em>
-              </template>
-              <b-dropdown-item href="#">Profile</b-dropdown-item>
-              <b-dropdown-item href="#">Signout</b-dropdown-item>
-            </b-nav-item-dropdown>
-          </b-navbar-nav>
-
-        </b-collapse>
-    </b-navbar>
+    <NavBar></NavBar>
     <Body></Body>
   </div>
 </template>
 
 <script>
 import Body from './components/Body.vue'
+import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'app',
   components: {
     Body,
+    NavBar
   }
 }
 </script>
