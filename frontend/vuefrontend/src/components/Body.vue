26d96dd8527a34f56e05005f36319d06794760a8
temidayo.o@ng.lopworks.com <temidayo.o@ng.lopworks.com>
2018-07-30 13:56:43 +0100

    More style changes

--- frontend/vuefrontend/src/components/Body.vue
+++ frontend/vuefrontend/src/components/Body.vue
@@ -4,13 +4,17 @@
 
     </b-jumbotron>
     <h1>Latest Movies</h1>
+
     <div>
+      <hr>
       <b-container fluid>
         <b-row>
           <b-col sm="6" md="4" v-for="movie in movies" :key="movie.id">
             <img :src="movie.image" alt="" srcset="">
             <p><a href="#">{{movie.title.toUpperCase()}}</a></p>
-            <b-button>Only ₦{{movie.price}}!</b-button>
+            <div class="buttons">
+              <b-button>Only ₦{{movie.price}}!</b-button>
+            </div>
           </b-col>
         </b-row>
       </b-container>
@@ -20,7 +24,7 @@
 
 <script>
 export default {
-  name: "Jumbotron",
+  name: "Body",
   data() {
     return {
       movies: [],
@@ -44,24 +48,45 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.content {
-  margin-top: 1%;
-  background-image: url("../assets/popcorn.jpg");
-}
-h1 {
-  text-align: center;
-  margin-bottom: 2%;
-}
-img{
-  max-height: 100%;
-  max-width: 100%;
-  margin-top: 2%;
-  margin-bottom: 2%;
-}
-button{
-  align-self: auto;
-}
-p{
-  text-align: center;
-}
+  .content {
+    margin-top: 1%;
+    background-image: url("../assets/popcorn.jpg");
+    background-size: 100%;
+  }
+  h1 {
+    text-align: center;
+    color: white;
+    margin-bottom: 2%;
+  }
+
+  img{
+    max-height: 100%;
+    max-width: 100%;
+    margin-top: 2%;
+    margin-bottom: 2%;
+  }
+
+  .buttons{
+    text-align: center
+  }
+
+  p{
+    text-align: center;
+  }
+
+  hr{
+    border: 0;
+    height: 1px;
+    background: #333;
+    background-image: linear-gradient(to right, #ccc, #333, #ccc);
+  }
+
+  .col-sm-6, .col-md-4{
+    margin-bottom: 1%;
+  }
+
+  .container-fluid{
+    padding-left: 5%;
+    padding-right: 5%;
+  }
 </style>
