38a61c1834b4a4e4405a09fbc73807d90e7638ad
temidayo.o@ng.lopworks.com <temidayo.o@ng.lopworks.com>
2018-07-30 16:58:58 +0100

    installed vue-router

--- frontend/vuefrontend/src/App.vue
+++ frontend/vuefrontend/src/App.vue
@@ -2,18 +2,21 @@
   <div id="body">
     <NavBar></NavBar>
     <Body></Body>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Body from './components/Body.vue'
 import NavBar from './components/NavBar.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'app',
   components: {
     Body,
-    NavBar
+    NavBar,
+    Footer
   }
 }
 </script>
---
+++ frontend/vuefrontend/src/components/Footer.vue
@@ -0,0 +1,47 @@
+<template>
+    <footer class="footer">
+        <div>
+            <nav class="links">
+                <ul class="list-inline">
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/about">About us</a>
+                    </li>
+                    <li>
+                        <a href="/contacts">Contacts</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </footer>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    .footer{
+        padding-top: 1%;
+        padding-bottom: 1%;
+        background: #364b59;
+        color: rgba(255, 255, 255, 0.6);
+    }
+    .links{
+        margin-left: 0;
+        display: inline-block;
+        margin: 0 auto;
+        margin-bottom: 30px;
+        font-size: 14px;
+        text-transform: uppercase;
+    }
+    .list-inline{
+        list-style: none;
+    }
+</style>
+
+
